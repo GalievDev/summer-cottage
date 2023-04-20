6aa2e84f04aabfd779be17973f8b7f6416e707b6
Galiev <ggvalion@gmail.com>
2023-04-20 10:52:58 +0500

    WaterCan completed

--- src/main/kotlin/dev/galiev/sc/items/custom/WaterCan.kt
+++ src/main/kotlin/dev/galiev/sc/items/custom/WaterCan.kt
@@ -2,22 +2,25 @@ package dev.galiev.sc.items.custom
 
 import dev.galiev.sc.SummerCottage
 import dev.galiev.sc.helper.NbtHelper
+import net.minecraft.block.CropBlock
+import net.minecraft.block.Fertilizable
 import net.minecraft.block.Material
 import net.minecraft.client.gui.screen.Screen
 import net.minecraft.client.item.TooltipContext
 import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.item.Item
 import net.minecraft.item.ItemStack
+import net.minecraft.item.ItemUsageContext
+import net.minecraft.server.world.ServerWorld
 import net.minecraft.sound.SoundEvents
 import net.minecraft.text.Text
-import net.minecraft.util.Formatting
-import net.minecraft.util.Hand
-import net.minecraft.util.TypedActionResult
-import net.minecraft.util.UseAction
+import net.minecraft.util.*
 import net.minecraft.util.hit.BlockHitResult
 import net.minecraft.util.hit.HitResult
+import net.minecraft.util.math.BlockPos
 import net.minecraft.world.RaycastContext
 import net.minecraft.world.World
+import net.minecraft.world.WorldEvents
 
 class WaterCan(settings: Settings?) : Item(settings) {
     override fun getUseAction(stack: ItemStack?): UseAction {
@@ -25,6 +28,7 @@ class WaterCan(settings: Settings?) : Item(settings) {
     }
     override fun use(world: World?, user: PlayerEntity?, hand: Hand?): TypedActionResult<ItemStack> {
         val stack = user?.getStackInHand(hand)
+        NbtHelper.setBoolean(stack, "Water", false)
         val trace: BlockHitResult = raycast(world, user, RaycastContext.FluidHandling.SOURCE_ONLY)
 
         if (trace.type != HitResult.Type.BLOCK){
@@ -34,30 +38,54 @@ class WaterCan(settings: Settings?) : Item(settings) {
         val pos = trace.blockPos
         val state = world?.getBlockState(pos)
 
-        if (state?.material == Material.WATER){
-            NbtHelper.setBoolean(stack, "Water", false)
-
-            if (!NbtHelper.getBoolean(stack, "Water")) {
-                NbtHelper.setInt(stack, "Liters", 100)
-            } else {
+        if (!NbtHelper.getBoolean(stack, "Water")) {
+            if (state?.material == Material.WATER) {
+                NbtHelper.setInt(stack, "Liters", 1000)
                 NbtHelper.setBoolean(stack, "Water", true)
+                user?.playSound(SoundEvents.ITEM_BUCKET_FILL, 1.0F, 1.0F)
+
+                SummerCottage.logger.info("Water: ${NbtHelper.getInt(stack, "Liters")}")
+
+                return TypedActionResult.success(stack)
             }
-            user?.playSound(SoundEvents.ITEM_BUCKET_FILL, 1.0F, 1.0F)
-
-            SummerCottage.logger.info("Water: ${NbtHelper.getInt(stack, "Liters")}")
-
-            return TypedActionResult.success(stack)
-        } else {
-            user?.playSound(SoundEvents.ENTITY_VILLAGER_NO, 1.0F, 1.0F)
         }
-
         return TypedActionResult.pass(stack)
     }
+
+    override fun useOnBlock(context: ItemUsageContext?): ActionResult {
+        val world = context?.world
+        val item = context?.stack
+        val blockPos = context?.blockPos
+        if (NbtHelper.getBoolean(item, "Liters") && useOnFertilizable(item, world, blockPos)){
+            if (!world?.isClient!!) {
+                world.syncWorldEvent(WorldEvents.BONE_MEAL_USED, blockPos, 0)
+                context.player?.playSound(SoundEvents.AMBIENT_UNDERWATER_ENTER, 1.0F, 1.0F)
+            }
+            return ActionResult.success(world.isClient)
+        }
+
+        return ActionResult.PASS
+    }
+
+    fun useOnFertilizable(stack: ItemStack?, world: World?, pos: BlockPos?): Boolean {
+        var blockState = world?.getBlockState(pos)
+        if (blockState?.block is Fertilizable && (blockState.block as Any as CropBlock).isFertilizable(world, pos, blockState, world?.isClient!!)) {
+            if (world is ServerWorld) {
+                if ((blockState.block as CropBlock).canGrow(world, world.random, pos, blockState)) {
+                    (blockState.block as CropBlock).grow(world, world.random, pos, blockState)
+                }
+                NbtHelper.setInt(stack, "Liters", NbtHelper.getInt(stack, "Liters") - 100)
+            }
+            return true
+        }
+        return false
+    }
+
 
     override fun appendTooltip(stack: ItemStack?, world: World?, tooltip: MutableList<Text>?, context: TooltipContext?) {
         val waterCount = NbtHelper.getInt(stack, "Liters")
         if (Screen.hasShiftDown()) {
-            tooltip?.add(Text.literal("$waterCount/100 Liters").formatted(Formatting.AQUA))
+            tooltip?.add(Text.literal("$waterCount/1000 Milliliters").formatted(Formatting.AQUA))
         } else {
             tooltip?.add(Text.literal("Press Shift for more info").formatted(Formatting.YELLOW))
         }
