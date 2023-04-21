0f4486d0732fe229fba1001d0018ddefda12be27
Galiev <ggvalion@gmail.com>
2023-04-21 16:53:04 +0500

    Breaking && plowing 3x3

--- src/main/kotlin/dev/galiev/sc/items/custom/Rake.kt
+++ src/main/kotlin/dev/galiev/sc/items/custom/Rake.kt
@@ -1,7 +1,6 @@
 package dev.galiev.sc.items.custom
 
 import dev.galiev.sc.mixin.CropBlockMixin
-import net.minecraft.block.BlockState
 import net.minecraft.block.Blocks
 import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.item.HoeItem
@@ -13,6 +12,7 @@ import net.minecraft.sound.SoundEvents
 import net.minecraft.util.ActionResult
 import net.minecraft.util.math.BlockPos
 import net.minecraft.world.World
+import kotlin.random.Random
 
 class Rake(material: ToolMaterial?, attackDamage: Int, attackSpeed: Float, settings: Settings?) : HoeItem(material, attackDamage, attackSpeed, settings) {
     override fun useOnBlock(context: ItemUsageContext?): ActionResult {
@@ -23,170 +23,62 @@ class Rake(material: ToolMaterial?, attackDamage: Int, attackSpeed: Float, setti
 
         if (blockState?.isIn(BlockTags.DIRT) == true) {
             world.setBlockState(pos, Blocks.FARMLAND.defaultState)
-            setBlocks(world, pos)
+            if (pos != null) {
+                setBlocks(pos.x, pos.y, pos.z, world)
+                setBlocks(pos.x + 1, pos.y, pos.z, world)
+                setBlocks(pos.x - 1, pos.y, pos.z, world)
+                setBlocks(pos.x, pos.y, pos.z + 1, world)
+                setBlocks(pos.x, pos.y, pos.z - 1, world)
+                setBlocks(pos.x + 1, pos.y, pos.z - 1, world)
+                setBlocks(pos.x + 1, pos.y, pos.z + 1, world)
+                setBlocks(pos.x - 1, pos.y, pos.z + 1, world)
+                setBlocks(pos.x - 1, pos.y, pos.z - 1, world)
+            }
 
             world.playSound(player, pos, SoundEvents.ITEM_HOE_TILL, SoundCategory.BLOCKS, 1.0F, 1.0F)
 
             if (!world.isClient && player != null){
                 context.stack.damage(1, player) { p: PlayerEntity -> p.sendToolBreakStatus(context.hand) }
             }
+
             ActionResult.success(world.isClient)
 
         } else if (blockState?.isIn(BlockTags.CROPS) == true) {
-            return if ((blockState.block as CropBlockMixin).getAgeInvoke(blockState) == 7) {
-                world.breakBlock(pos, true)
-                breakBlocks(pos, world)
-                if (!world.isClient && player != null){
-                    context.stack.damage(1, player) { p: PlayerEntity -> p.sendToolBreakStatus(context.hand) }
-                }
-                ActionResult.SUCCESS
-            } else {
-                ActionResult.PASS
+            if (pos != null) {
+                breakBlocks(pos.x, pos.y, pos.z, world)
+                breakBlocks(pos.x + 1, pos.y, pos.z, world)
+                breakBlocks(pos.x - 1, pos.y, pos.z, world)
+                breakBlocks(pos.x, pos.y, pos.z + 1, world)
+                breakBlocks(pos.x, pos.y, pos.z - 1, world)
+                breakBlocks(pos.x + 1, pos.y, pos.z - 1, world)
+                breakBlocks(pos.x + 1, pos.y, pos.z + 1, world)
+                breakBlocks(pos.x - 1, pos.y, pos.z + 1, world)
+                breakBlocks(pos.x - 1, pos.y, pos.z - 1, world)
             }
+
+            if (!world.isClient && player != null){
+                context.stack.damage(Random.nextInt(2, 9), player) { p: PlayerEntity -> p.sendToolBreakStatus(context.hand) }
+            }
+
+            return ActionResult.success(world.isClient)
         }
 
         return ActionResult.PASS
     }
 
-    fun breakBlocks(pos: BlockPos?, world: World): ActionResult {
-        var pos2: BlockPos.Mutable
-        var blockState2: BlockState
-        if (pos != null) {
-            pos2 = BlockPos.Mutable(pos.x+1, pos.y, pos.z)
-            blockState2 = world.getBlockState(pos2)
-            if (blockState2.isIn(BlockTags.CROPS) && (blockState2.block as CropBlockMixin).getAgeInvoke(blockState2) == 7){
-                world.breakBlock(pos2, true)
-            } else {
-                return ActionResult.PASS
-            }
-
-            pos2 = BlockPos.Mutable(pos.x-1, pos.y, pos.z)
-            blockState2 = world.getBlockState(pos2)
-            if (blockState2.isIn(BlockTags.CROPS) && (blockState2.block as CropBlockMixin).getAgeInvoke(blockState2) == 7){
-                world.breakBlock(pos2, true)
-            } else {
-                return ActionResult.PASS
-            }
-
-            pos2 = BlockPos.Mutable(pos.x, pos.y, pos.z+1)
-            blockState2 = world.getBlockState(pos2)
-            if (blockState2.isIn(BlockTags.CROPS) && (blockState2.block as CropBlockMixin).getAgeInvoke(blockState2) == 7){
-                world.breakBlock(pos2, true)
-            } else {
-                return ActionResult.PASS
-            }
-
-            pos2 = BlockPos.Mutable(pos.x, pos.y, pos.z-1)
-            blockState2 = world.getBlockState(pos2)
-            if (blockState2.isIn(BlockTags.CROPS) && (blockState2.block as CropBlockMixin).getAgeInvoke(blockState2) == 7){
-                world.breakBlock(pos2, true)
-            } else {
-                return ActionResult.PASS
-            }
-
-            pos2 = BlockPos.Mutable(pos.x+1, pos.y, pos.z-1)
-            blockState2 = world.getBlockState(pos2)
-            if (blockState2.isIn(BlockTags.CROPS) && (blockState2.block as CropBlockMixin).getAgeInvoke(blockState2) == 7){
-                world.breakBlock(pos2, true)
-            } else {
-                return ActionResult.PASS
-            }
-
-            pos2 = BlockPos.Mutable(pos.x+1, pos.y, pos.z+1)
-            blockState2 = world.getBlockState(pos2)
-            if (blockState2.isIn(BlockTags.CROPS) && (blockState2.block as CropBlockMixin).getAgeInvoke(blockState2) == 7){
-                world.breakBlock(pos2, true)
-            } else {
-                return ActionResult.PASS
-            }
-
-            pos2 = BlockPos.Mutable(pos.x-1, pos.y, pos.z+1)
-            blockState2 = world.getBlockState(pos2)
-            if (blockState2.isIn(BlockTags.CROPS) && (blockState2.block as CropBlockMixin).getAgeInvoke(blockState2) == 7){
-                world.breakBlock(pos2, true)
-            } else {
-                return ActionResult.PASS
-            }
-
-            pos2 = BlockPos.Mutable(pos.x-1, pos.y, pos.z-1)
-            blockState2 = world.getBlockState(pos2)
-            if (blockState2.isIn(BlockTags.CROPS) && (blockState2.block as CropBlockMixin).getAgeInvoke(blockState2) == 7){
-                world.breakBlock(pos2, true)
-            } else {
-                return ActionResult.PASS
-            }
+    fun breakBlocks(x: Int, y: Int, z: Int, world: World) {
+        val pos: BlockPos.Mutable = BlockPos.Mutable(x,y,z)
+        val blockState = world.getBlockState(pos)
+        if (blockState.isIn(BlockTags.CROPS) && (blockState.block as CropBlockMixin).getAgeInvoke(blockState) == 7){
+            world.breakBlock(pos, true)
         }
-        return ActionResult.PASS
     }
 
-    fun setBlocks(world: World, pos: BlockPos?): ActionResult {
-        var pos2: BlockPos.Mutable
-        var blockState2: BlockState
-        if (pos != null) {
-            pos2 = BlockPos.Mutable(pos.x+1, pos.y, pos.z)
-            blockState2 = world.getBlockState(pos2)
-            if (blockState2.isIn(BlockTags.DIRT)){
-                world.setBlockState(pos2, Blocks.FARMLAND.defaultState)
-            } else {
-                return ActionResult.PASS
-            }
-
-            pos2 = BlockPos.Mutable(pos.x-1, pos.y, pos.z)
-            blockState2 = world.getBlockState(pos2)
-            if (blockState2.isIn(BlockTags.DIRT)){
-                world.setBlockState(pos2, Blocks.FARMLAND.defaultState)
-            } else {
-                return ActionResult.PASS
-            }
-
-            pos2 = BlockPos.Mutable(pos.x, pos.y, pos.z+1)
-            blockState2 = world.getBlockState(pos2)
-            if (blockState2.isIn(BlockTags.DIRT)){
-                world.setBlockState(pos2, Blocks.FARMLAND.defaultState)
-            } else {
-                return ActionResult.PASS
-            }
-
-            pos2 = BlockPos.Mutable(pos.x, pos.y, pos.z-1)
-            blockState2 = world.getBlockState(pos2)
-            if (blockState2.isIn(BlockTags.DIRT)){
-                world.setBlockState(pos2, Blocks.FARMLAND.defaultState)
-            } else {
-                return ActionResult.PASS
-            }
-
-            pos2 = BlockPos.Mutable(pos.x+1, pos.y, pos.z-1)
-            blockState2 = world.getBlockState(pos2)
-            if (blockState2.isIn(BlockTags.DIRT)){
-                world.setBlockState(pos2, Blocks.FARMLAND.defaultState)
-            } else {
-                return ActionResult.PASS
-            }
-
-            pos2 = BlockPos.Mutable(pos.x+1, pos.y, pos.z+1)
-            blockState2 = world.getBlockState(pos2)
-            if (blockState2.isIn(BlockTags.DIRT)){
-                world.setBlockState(pos2, Blocks.FARMLAND.defaultState)
-            } else {
-                return ActionResult.PASS
-            }
-
-            pos2 = BlockPos.Mutable(pos.x-1, pos.y, pos.z+1)
-            blockState2 = world.getBlockState(pos2)
-            if (blockState2.isIn(BlockTags.DIRT)){
-                world.setBlockState(pos2, Blocks.FARMLAND.defaultState)
-            } else {
-                return ActionResult.PASS
-            }
-
-            pos2 = BlockPos.Mutable(pos.x-1, pos.y, pos.z-1)
-            blockState2 = world.getBlockState(pos2)
-            if (blockState2.isIn(BlockTags.DIRT)){
-                world.setBlockState(pos2, Blocks.FARMLAND.defaultState)
-            } else {
-                return ActionResult.PASS
-            }
+    fun setBlocks(x: Int, y: Int, z: Int, world: World) {
+        val pos: BlockPos.Mutable = BlockPos.Mutable(x,y,z)
+        val blockState = world.getBlockState(pos)
+        if(blockState.isIn(BlockTags.DIRT)){
+            world.setBlockState(pos, Blocks.FARMLAND.defaultState)
         }
-        return ActionResult.PASS
     }
 }
