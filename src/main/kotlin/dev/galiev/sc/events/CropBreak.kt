c1891fbefcee8b5cb1706b2ffd9c8598f69618ab
Galiev <ggvalion@gmail.com>
2023-04-24 16:04:16 +0500

    Events

--- src/main/kotlin/dev/galiev/sc/events/CropBreak.kt
+++ src/main/kotlin/dev/galiev/sc/events/CropBreak.kt
@@ -1,12 +1,11 @@
 package dev.galiev.sc.events
 
 import dev.galiev.sc.items.IRegistry
-import dev.galiev.sc.mixin.CropBlockMixin
 import net.fabricmc.fabric.api.event.player.AttackBlockCallback
-import net.minecraft.block.CropBlock
 import net.minecraft.entity.EquipmentSlot
 import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.item.ItemStack
+import net.minecraft.item.Items
 import net.minecraft.util.ActionResult
 import net.minecraft.util.Hand
 import net.minecraft.util.math.BlockPos
@@ -27,12 +26,10 @@ object CropBreak: AttackBlockCallback {
                 player.getEquippedStack(EquipmentSlot.CHEST) == IRegistry.GARDENER_SHIRT?.defaultStack &&
                 player.getEquippedStack(EquipmentSlot.LEGS) == IRegistry.GARDENER_LEGGINGS?.defaultStack){
                 val blockState = world.getBlockState(pos)
-                val id = blockState.block.lootTableId
-                if ((blockState.block is CropBlock) && (blockState.block as CropBlockMixin).getAgeInvoke(blockState) == 7){
-                    val items = blockState.block.getPickStack(world.getWorldChunk(pos), pos, blockState).item
-                    val stack: ItemStack = ItemStack(items, Random.nextInt(1, 7))
-                    player.giveItemStack(stack)
-                }
+                val items = Items.EGG
+                val stack = ItemStack(items, Random.nextInt(1, 7))
+                player.giveItemStack(stack)
+
                 return ActionResult.SUCCESS
             }
         }
