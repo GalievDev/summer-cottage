e68d0de648117bc44969b0ec84806a62da4da259
Galiev <ggvalion@gmail.com>
2023-05-10 19:32:27 +0500

    Chair Entity

--- src/main/kotlin/dev/galiev/sc/blocks/custom/FoldingChair.kt
+++ src/main/kotlin/dev/galiev/sc/blocks/custom/FoldingChair.kt
@@ -1,5 +1,6 @@
 package dev.galiev.sc.blocks.custom
 
+import dev.galiev.sc.helper.EntityTypeRegistry
 import net.fabricmc.fabric.api.`object`.builder.v1.block.FabricBlockSettings
 import net.minecraft.block.Block
 import net.minecraft.block.BlockState
@@ -9,6 +10,7 @@ import net.minecraft.util.ActionResult
 import net.minecraft.util.Hand
 import net.minecraft.util.hit.BlockHitResult
 import net.minecraft.util.math.BlockPos
+import net.minecraft.util.math.Vec3d
 import net.minecraft.world.World
 
 class FoldingChair(settings: Settings = FabricBlockSettings.of(Material.WOOD).nonOpaque()) : Block(settings) {
@@ -21,6 +23,26 @@ class FoldingChair(settings: Settings = FabricBlockSettings.of(Material.WOOD).no
         hand: Hand?,
         hit: BlockHitResult?
     ): ActionResult {
-        return ActionResult.SUCCESS
+        if (world?.isClient!!) {
+            return ActionResult.PASS
+        }
+        if (!player?.isSneaking!!) {
+            return spawnChair(world, player, pos)
+        }
+        return ActionResult.PASS
+    }
+
+    fun spawnChair(world: World?, player: PlayerEntity?, pos: BlockPos?): ActionResult {
+        val chair = EntityTypeRegistry.CHAIR_ENTITY.create(world)
+        if (chair != null) {
+            val newPos = Vec3d(pos?.x!! + 0.5, pos.y + 0.5, pos.z + 0.5)
+            chair.updatePosition(newPos.x, newPos.y, newPos.z)
+            world?.spawnEntity(chair)
+            player?.startRiding(chair)
+
+            return ActionResult.SUCCESS
+        }
+
+        return ActionResult.PASS
     }
 }
