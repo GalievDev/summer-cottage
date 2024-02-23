6bc668ad4bec6521b9972c7295f5169950472a3b
GalievDev <ggvalion@gmail.com>
2024-02-23 17:44:03 +0300

    Iteration logic fix

--- src/main/kotlin/dev/galiev/sc/items/custom/Rake.kt
+++ src/main/kotlin/dev/galiev/sc/items/custom/Rake.kt
@@ -1,9 +1,11 @@
 package dev.galiev.sc.items.custom
 
+import dev.galiev.sc.SummerCottage.RANDOM
 import dev.galiev.sc.events.SeedHarvestEvent
 import dev.galiev.sc.mixin.CropBlockMixin
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings
 import net.minecraft.block.Blocks
+import net.minecraft.block.CropBlock
 import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.item.HoeItem
 import net.minecraft.item.ItemUsageContext
@@ -13,10 +15,9 @@ import net.minecraft.sound.SoundCategory
 import net.minecraft.sound.SoundEvents
 import net.minecraft.util.ActionResult
 import net.minecraft.util.math.BlockPos
-import net.minecraft.world.World
-import kotlin.random.Random
 
-class Rake() : HoeItem(ToolMaterials.IRON, 1, 1F, FabricItemSettings()) {
+
+class Rake : HoeItem(ToolMaterials.IRON, 1, 1F, FabricItemSettings()) {
     override fun useOnBlock(context: ItemUsageContext?): ActionResult {
         val world = context?.world
         val pos = context?.blockPos
@@ -24,64 +25,45 @@ class Rake() : HoeItem(ToolMaterials.IRON, 1, 1F, FabricItemSettings()) {
         val player = context?.player
 
         if (blockState?.isIn(BlockTags.DIRT) == true) {
-            world.setBlockState(pos, Blocks.FARMLAND.defaultState)
             if (pos != null) {
-                setBlocks(pos.x, pos.y, pos.z, world)
-                setBlocks(pos.x + 1, pos.y, pos.z, world)
-                setBlocks(pos.x - 1, pos.y, pos.z, world)
-                setBlocks(pos.x, pos.y, pos.z + 1, world)
-                setBlocks(pos.x, pos.y, pos.z - 1, world)
-                setBlocks(pos.x + 1, pos.y, pos.z - 1, world)
-                setBlocks(pos.x + 1, pos.y, pos.z + 1, world)
-                setBlocks(pos.x - 1, pos.y, pos.z + 1, world)
-                setBlocks(pos.x - 1, pos.y, pos.z - 1, world)
+                for (targetPos in BlockPos.iterate(pos.add(-1, -1, -1), pos.add(1, 1, 1))) {
+                    if (world.getBlockState(targetPos.up()).isOf(Blocks.GRASS)) {
+                        world.breakBlock(targetPos.up(), true)
+                    }
+                    if (world.getBlockState(targetPos).isIn(BlockTags.DIRT) && world.isAir(targetPos.up())) {
+                        world.setBlockState(targetPos, Blocks.FARMLAND.defaultState)
+                    }
+                }
+
+                world.playSound(player, pos, SoundEvents.ITEM_HOE_TILL, SoundCategory.BLOCKS, 1.0F, 1.0F)
+
+                if (!world.isClient && player != null) {
+                    context.stack.damage(1, player) { p: PlayerEntity -> p.sendToolBreakStatus(context.hand) }
+                }
+
+                ActionResult.success(world.isClient)
             }
+        }
 
-            world.playSound(player, pos, SoundEvents.ITEM_HOE_TILL, SoundCategory.BLOCKS, 1.0F, 1.0F)
-
-            if (!world.isClient && player != null){
-                context.stack.damage(1, player) { p: PlayerEntity -> p.sendToolBreakStatus(context.hand) }
-            }
-
-            ActionResult.success(world.isClient)
-
-        } else if (blockState?.isIn(BlockTags.CROPS) == true) {
+        else if (blockState?.isIn(BlockTags.CROPS) == true) {
             if (pos != null) {
-                breakBlocks(pos.x, pos.y, pos.z, world)
-                breakBlocks(pos.x + 1, pos.y, pos.z, world)
-                breakBlocks(pos.x - 1, pos.y, pos.z, world)
-                breakBlocks(pos.x, pos.y, pos.z + 1, world)
-                breakBlocks(pos.x, pos.y, pos.z - 1, world)
-                breakBlocks(pos.x + 1, pos.y, pos.z - 1, world)
-                breakBlocks(pos.x + 1, pos.y, pos.z + 1, world)
-                breakBlocks(pos.x - 1, pos.y, pos.z + 1, world)
-                breakBlocks(pos.x - 1, pos.y, pos.z - 1, world)
-            }
+                for (targetPos in BlockPos.iterate(pos.add(-1, -1, -1), pos.add(1, 1, 1))) {
+                    if ((blockState.block as CropBlockMixin).getAgeInvoke(blockState) == CropBlock.MAX_AGE){
+                        world.breakBlock(pos, true)
+                    }
+                }
 
-            if (!world.isClient && player != null){
-                context.stack.damage(Random.nextInt(2, 9), player) { p: PlayerEntity -> p.sendToolBreakStatus(context.hand) }
-                SeedHarvestEvent.afterBlockBreak(world, player, pos, blockState, null)
-            }
+                if (!world.isClient && player != null) {
+                    context.stack.damage(RANDOM.nextInt(2, 9), player) { p: PlayerEntity ->
+                        p.sendToolBreakStatus(context.hand)
+                    }
+                    SeedHarvestEvent.afterBlockBreak(world, player, pos, blockState, null)
+                }
 
-            return ActionResult.success(world.isClient)
+                return ActionResult.success(world.isClient)
+            }
         }
 
         return ActionResult.PASS
-    }
-
-    private fun breakBlocks(x: Int, y: Int, z: Int, world: World) {
-        val pos: BlockPos.Mutable = BlockPos.Mutable(x,y,z)
-        val blockState = world.getBlockState(pos)
-        if (blockState.isIn(BlockTags.CROPS) && (blockState.block as CropBlockMixin).getAgeInvoke(blockState) == 7){
-            world.breakBlock(pos, true)
-        }
-    }
-
-    private fun setBlocks(x: Int, y: Int, z: Int, world: World) {
-        val pos: BlockPos.Mutable = BlockPos.Mutable(x,y,z)
-        val blockState = world.getBlockState(pos)
-        if(blockState.isIn(BlockTags.DIRT)){
-            world.setBlockState(pos, Blocks.FARMLAND.defaultState)
-        }
     }
 }
