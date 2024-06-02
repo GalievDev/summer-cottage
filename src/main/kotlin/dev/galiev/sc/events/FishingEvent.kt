acf5116c8fab19d39bd4b96a64989eba94822823
galiev <ggvalion@gmail.com>
2024-06-02 16:31:09 +0300

    Fishing Event

--- src/main/kotlin/dev/galiev/sc/events/FishingEvent.kt
+++ src/main/kotlin/dev/galiev/sc/events/FishingEvent.kt
@@ -1,12 +1,13 @@
 package dev.galiev.sc.events
 
-import dev.galiev.sc.SummerCottage.RANDOM
 import dev.galiev.sc.items.ItemsRegistry
 import net.fabricmc.fabric.api.event.player.UseItemCallback
 import net.minecraft.entity.EquipmentSlot
+import net.minecraft.entity.effect.StatusEffectInstance
+import net.minecraft.entity.effect.StatusEffects
 import net.minecraft.entity.player.PlayerEntity
+import net.minecraft.item.FishingRodItem
 import net.minecraft.item.ItemStack
-import net.minecraft.item.Items
 import net.minecraft.util.Hand
 import net.minecraft.util.TypedActionResult
 import net.minecraft.world.World
@@ -16,18 +17,15 @@ object FishingEvent: UseItemCallback {
         val stack = player?.mainHandStack
         if (stack == ItemStack.EMPTY) return TypedActionResult.fail(stack)
 
-        if (stack?.item == Items.FISHING_ROD) {
-            val helmet = player?.getEquippedStack(EquipmentSlot.HEAD)?.item
-            val chest = player?.getEquippedStack(EquipmentSlot.CHEST)?.item
-            val legs = player?.getEquippedStack(EquipmentSlot.LEGS)?.item
+        if (stack?.item is FishingRodItem && player.fishHook != null) {
+            val helmet = player.getEquippedStack(EquipmentSlot.HEAD)?.item
+            val chest = player.getEquippedStack(EquipmentSlot.CHEST)?.item
+            val legs = player.getEquippedStack(EquipmentSlot.LEGS)?.item
 
             if (helmet == ItemsRegistry.FISHERMAN_HAT && chest == ItemsRegistry.FISHERMAN_SHIRT && legs == ItemsRegistry.FISHERMAN_LEGGINGS) {
-                if (RANDOM.nextInt(1, 100) <= 49) {
-
-                }
+                player.addStatusEffect(StatusEffectInstance(StatusEffects.LUCK))
             }
-
-            return TypedActionResult.success(stack)
+            return TypedActionResult.pass(stack)
         }
         return TypedActionResult.pass(stack)
     }
