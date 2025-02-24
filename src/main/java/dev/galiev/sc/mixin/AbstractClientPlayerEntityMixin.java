49c6114ccd8e0c9dedce1b6f917907009021aeaf
Vazir Vadirov <ggvalion@gmail.com>
2025-02-24 15:10:44 +0500

    Gamemode bug fixed

--- src/main/java/dev/galiev/sc/mixin/AbstractClientPlayerEntityMixin.java
+++ src/main/java/dev/galiev/sc/mixin/AbstractClientPlayerEntityMixin.java
@@ -3,18 +3,26 @@ package dev.galiev.sc.mixin;
 import com.mojang.authlib.GameProfile;
 import dev.galiev.sc.items.ItemsRegistry;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.PlayerListEntry;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
+import net.minecraft.world.GameMode;
 import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import net.minecraft.client.network.AbstractClientPlayerEntity;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(AbstractClientPlayerEntity.class)
-public class AbstractClientPlayerEntityMixin extends PlayerEntity {
+public abstract class AbstractClientPlayerEntityMixin extends PlayerEntity {
+
+    @Shadow
+    protected abstract @Nullable PlayerListEntry getPlayerListEntry();
+
 
     public AbstractClientPlayerEntityMixin(World world, BlockPos pos, float yaw, GameProfile gameProfile) {
         super(world, pos, yaw, gameProfile);
@@ -37,11 +45,13 @@ public class AbstractClientPlayerEntityMixin extends PlayerEntity {
 
     @Override
     public boolean isSpectator() {
-        return false;
+        var playerEntry = this.getPlayerListEntry();
+        return playerEntry != null && playerEntry.getGameMode() == GameMode.SPECTATOR;
     }
 
     @Override
     public boolean isCreative() {
-        return false;
+        var playerEntry = this.getPlayerListEntry();
+        return playerEntry != null && playerEntry.getGameMode() == GameMode.CREATIVE;
     }
 }
