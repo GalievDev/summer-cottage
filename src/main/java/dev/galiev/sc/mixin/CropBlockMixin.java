997de98343e937ee10ed82053f083d8d78af8aaf
Vazir Vadirov <ggvalion@gmail.com>
2025-03-10 20:17:04 +0500

    Removed unnecessary mixin call

--- src/main/java/dev/galiev/sc/mixin/CropBlockMixin.java
+++ src/main/java/dev/galiev/sc/mixin/CropBlockMixin.java
@@ -1,13 +1,9 @@
 package dev.galiev.sc.mixin;
 
-import net.minecraft.block.BlockState;
 import net.minecraft.block.CropBlock;
 import net.minecraft.block.Fertilizable;
 import net.minecraft.block.PlantBlock;
-import net.minecraft.state.property.IntProperty;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.gen.Invoker;
 
 @Mixin(CropBlock.class)
 public abstract class CropBlockMixin extends PlantBlock implements Fertilizable {
