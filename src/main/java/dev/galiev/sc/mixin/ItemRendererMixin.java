ffefa984aea023aea00392e8535f10337cd6503c
Vazir Vadirov <ggvalion@gmail.com>
2025-03-21 11:16:45 +0500

    2D models for rake and water can

--- src/main/java/dev/galiev/sc/mixin/ItemRendererMixin.java
+++ src/main/java/dev/galiev/sc/mixin/ItemRendererMixin.java
@@ -16,9 +16,15 @@ import org.spongepowered.asm.mixin.injection.ModifyVariable;
 @Mixin(ItemRenderer.class)
 public abstract class ItemRendererMixin {
     @ModifyVariable(method = "renderItem", at = @At(value = "HEAD"), argsOnly = true)
-    public BakedModel useRubyStaffModel(BakedModel value, ItemStack stack, ModelTransformationMode renderMode, boolean leftHanded, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, int overlay) {
+    public BakedModel use3DModel(BakedModel value, ItemStack stack, ModelTransformationMode renderMode, boolean leftHanded, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, int overlay) {
         if (stack.isOf(ItemsRegistry.INSTANCE.getDARTS()) && renderMode != ModelTransformationMode.GUI) {
             return ((ItemRendererAccessor) this).sc$getModels().getModelManager().getModel(new ModelIdentifier(SummerCottage.MOD_ID, "darts_3d", "inventory"));
+        }
+        if (stack.isOf(ItemsRegistry.INSTANCE.getWATER_CAN()) && renderMode != ModelTransformationMode.GUI) {
+            return ((ItemRendererAccessor) this).sc$getModels().getModelManager().getModel(new ModelIdentifier(SummerCottage.MOD_ID, "water_can_3d", "inventory"));
+        }
+        if (stack.isOf(ItemsRegistry.INSTANCE.getRAKE()) && renderMode != ModelTransformationMode.GUI) {
+            return ((ItemRendererAccessor) this).sc$getModels().getModelManager().getModel(new ModelIdentifier(SummerCottage.MOD_ID, "rake_3d", "inventory"));
         }
         return value;
     }
